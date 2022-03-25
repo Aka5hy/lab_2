55fcda5ea9583ab774f06ef5f672a3d748d3e67b
Djamalutdin <Dj@gmail.com>
2022-03-25 21:57:45 +0300

    code; print Max

--- main.cpp
+++ main.cpp
@@ -17,7 +17,8 @@ int main()
 	cout << "A + B = " << A + B << endl
 		 << "A - B = " << A - B << endl
 	     << "A * B = " << A * B << endl
-	     << "A * B = " << A * B << endl;
+	     << "A * B = " << A * B << endl
+		 <<"Max";
 
 	return 1;
 }
